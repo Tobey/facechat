be805d2e28beceef02a36ed243d8943f4d0207ea
Tobey Asinugo <tobey.asinugo@gmail.com>
2020-04-14 20:19:52 +0100

    fix load

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -24,36 +24,6 @@ Amplify.configure({
 });
 
 
-const mountedChat = (that) => {
-      if (that.state.getOpenTokSession && that.state.facechatToken){
-        let to =  'cGFydG5lcl9pZD00NjUzMTUzMiZzaWc9MTY2ZmE0Yzk5MTFiYjM5YzI2YzI1MTY1OTYzOTJhNjlmNzU4ZmU4MTpzZXNzaW9uX2lkPTJfTVg0ME5qVXpNVFV6TW41LU1UVTROamczT0RJeE1UUXhOWDVZTlZNMFdteG1NelpNYzNCeWNVNHpRMWhLY1Zoc1JuQi1mZyZjcmVhdGVfdGltZT0xNTg2ODg4MjcxJm5vbmNlPTAuMTc1MDM5NTgxMjEyMTU5JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1ODY5NzQ2NzEmY29ubmVjdGlvbl9kYXRhPXVzZXJuYW1lJTNEYm9iJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9'
-        return (
-        <div>
-           <OTSession
-              apiKey={config.API_KEY}
-              sessionId={"2_MX40NjUzMTUzMn5-MTU4Njg3ODIxMTQxNX5YNVM0WmxmMzZMc3BycU4zQ1hKcVhsRnB-fg"}
-              token={that.state.facechatToken || to}
-              eventHandlers={that.sessionEvents}
-              onError={that.onError}
-          >
-
-            <OTStreams>
-              <Subscriber/>
-          </OTStreams>
-          <Publisher/>
-        </OTSession>
-        </div>
-        )
-
-      }
-      else {
-          return <div>loading..</div>
-      }
-    
-            
- 
-}
-
 class Chat extends React.Component {
   state = {
     facechatSession: null,
@@ -69,26 +39,46 @@ class Chat extends React.Component {
   componentDidMount() {
     console.log("Mounted s")
     console.log('getOpenTokSession query')
-    let s = (async () => {
-      let data = await API.graphql(graphqlOperation(getOpenTokSession));
-      console.log(data)
-      console.log("getOpenTokSession")
-      console.log(data.data.getOpenTokSession)
-      this.setState({facechatSession: data.data.getOpenTokSession})
-    })();
+      let s = (async () => {
+        let data = await API.graphql(graphqlOperation(getOpenTokSession));
+        console.log(data)
+        console.log("getOpenTokSession")
+        console.log(data.data.getOpenTokSession)
+        this.setState({facechatSession: data.data.getOpenTokSession})
+      })();
 
-    s = (async () => {
-      let data = await API.graphql(graphqlOperation(getPublishToken));
-        this.setState({facechatToken: data.data.getPublishToken})
-        console.log("getPublishToken ")
-        console.log(data.data.getPublishToken)
-    })();
+      s = (async () => {
+        let data = await API.graphql(graphqlOperation(getPublishToken));
+          this.setState({facechatToken: data.data.getPublishToken})
+          console.log("getPublishToken ")
+          console.log(data.data.getPublishToken)
+      })();
 
-    console.log('done with mount')
-  }
+      console.log('done  with mount')
+    }
     
   render() {
-    return mountedChat(this)
+
+    if (this.state.facechatSession == null ){
+      return <div>Loading..</div>
+    } else {
+      let to =  'cGFydG5lcl9pZD00NjUzMTUzMiZzaWc9MTY2ZmE0Yzk5MTFiYjM5YzI2YzI1MTY1OTYzOTJhNjlmNzU4ZmU4MTpzZXNzaW9uX2lkPTJfTVg0ME5qVXpNVFV6TW41LU1UVTROamczT0RJeE1UUXhOWDVZTlZNMFdteG1NelpNYzNCeWNVNHpRMWhLY1Zoc1JuQi1mZyZjcmVhdGVfdGltZT0xNTg2ODg4MjcxJm5vbmNlPTAuMTc1MDM5NTgxMjEyMTU5JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1ODY5NzQ2NzEmY29ubmVjdGlvbl9kYXRhPXVzZXJuYW1lJTNEYm9iJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9'
+      return (
+        <OTSession
+        apiKey={config.API_KEY}
+        sessionId={"2_MX40NjUzMTUzMn5-MTU4Njg3ODIxMTQxNX5YNVM0WmxmMzZMc3BycU4zQ1hKcVhsRnB-fg"}
+        token={this.state.facechatToken || to}
+        eventHandlers={this.sessionEvents}
+        onError={this.onError}
+    >
+
+          <OTStreams>
+            <Subscriber/>
+        </OTStreams>
+        <Publisher/>
+      </OTSession>
+      )
+    }
     }
 
 }
